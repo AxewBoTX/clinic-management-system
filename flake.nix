108a73f8f0aeb5d10962483a5e3e397c5e88683e
lovedeepsingh-07 <lovedeepsinghbrar07@gmail.com>
2026-03-01 18:53:43 +1100

    feat: basic setup

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "clinic-management-system";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/ac055f38c798b0d87695240c7b761b82fc7e5bc2";
+  };
+  outputs = {...} @ inputs: let
+    system = "x86_64-linux";
+    pkgs = import inputs.nixpkgs {
+      inherit system;
+    };
+    python = rec {
+      version = "311";
+      pkg = pkgs."python${version}";
+      env = pkg.withPackages (pp:
+        with pp; [
+          pkg
+          tkinter
+        ]);
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      nativeBuildInputs = [
+        pkgs.alejandra
+        pkgs.pkg-config
+        python.env
+        pkgs.black
+      ];
+      buildInputs = [];
+      shellHook = "zsh";
+    };
+  };
+}
