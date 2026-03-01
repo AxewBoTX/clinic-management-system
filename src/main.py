badcd75abd719e0a0d295fc6a06546d8648724f9
Lovedeep <84615670+AxewBoTX@users.noreply.github.com>
2026-03-01 18:56:13 +1100

    Change print message in main function

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 def main():
-    print("Hello, World!")
+    # In this commit, I just add this commit and change the print message
+    print("this is the new message")
 
 if __name__ == "__main__":
     main()
